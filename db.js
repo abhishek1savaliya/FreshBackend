a2dfd4bddce3f3ae0c49b3dc932a625ef5b8debe
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-11-20 23:06:38 +0530

    fix issue

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongooseUri = "mongodb+srv://Abhi:Pj8hC58xIWZvaaO7@cluster0.31pgkje.mongodb.net/inotebook";
+const mongooseUri = "mongodb+srv://abhisheksavaliya555:ELW4NgCLMTWt1JCT@cluster0.i7awtq3.mongodb.net/inotebook";
 
 const connectToMongo = ()=>{
     mongoose.connect(mongooseUri); 
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const connectToMongo = require('./db');
-const express = require('express')
-var cors = require('cors')
- 
+const express = require('express');
+const cors = require('cors');
+
 connectToMongo();
-const app = express()
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin:"https://main.d36kv7hoeefvni.amplifyapp.com",
-}))
+  origin: "*",
+}));
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/notes',require('./routes/notes.js'))
-
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes.js'));
 
 app.listen(port, () => {
-  console.log(`iNotebook Backend listening on port ${port}`)
-})
+  console.log(`iNotebook Backend listening on port ${port}`);
+});
