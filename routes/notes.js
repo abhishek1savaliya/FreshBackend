48b20a01e2ed62daa90c89e1d673a9a84cfe5d0e
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-12-29 19:04:47 +0530

    update the code

--- routes/notes.js
+++ routes/notes.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const fetchuser = require('../middleware/fetchuser');
 const router = express.Router();
-const Note = require('../models/Note');
-const { body, validationResult } = require('express-validator');
 const { getAllNote, updateNote, addNote, deleteNote } = require('../controller.js/note.controller');
 
 router.get('/fetchallnotes', fetchuser, getAllNote)
 
 router.post('/addnote', fetchuser, addNote);
 
-
 router.put('/updatenote/:id', fetchuser, updateNote)
-
 
 router.delete('/deletenote/:id', fetchuser, deleteNote)
 
