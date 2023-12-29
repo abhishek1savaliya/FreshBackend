3e8251b191dca371fb3e2277a3adff14219f17f2
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-12-29 19:05:38 +0530

    update the code

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const { body, validationResult } = require('express-validator');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const fetchuser = require('../middleware/fetchuser');
 const user = require('../controller.js/auth.controller');
 
-const JWT_SECRET = "Abhishekhasenoughpowertolearnmore";
-
 router.post('/createuser', user.createUser);
 
-
 router.post('/login', user.login);
-
 
 router.post('/getuser', fetchuser, user.getUser);
 
